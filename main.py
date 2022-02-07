6a181312af452a1600c7af8a9fbeb4038b2eb4af
njavor <nikolettjavor@gmail.com>
2022-02-08 00:18:36 +0100

    done

--- main.py
+++ main.py
@@ -1,5 +1,11 @@
-with open("input.txt","r",encoding="utf8") as f:
-	for sor in f:
-		print(f"print({sor})")
-    
-main.py > out.txt
+def ToPrint():
+	with open("input.txt","r",encoding="utf8") as f:
+		with open("output.txt", "a", encoding="utf8") as g:
+			for sor in f:
+				s = sor.replace("\n", "")
+				s= s.replace("\t", " ")
+				print(f"print(\"{s}\")")
+				g.write(f"print(\"{s}\")\n")
+			g.close
+
+ToPrint()
