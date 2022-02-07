7a7291563b27000a05be67549b163dae7b97fa01
njavor <nikolettjavor@gmail.com>
2022-02-08 00:58:04 +0100

    + c#

--- main.py
+++ main.py
@@ -1,11 +1,19 @@
-def ToPrint():
+def ToPrint(nyelv):
 	with open("input.txt","r",encoding="utf8") as f:
 		with open("output.txt", "a", encoding="utf8") as g:
-			for sor in f:
-				s = sor.replace("\n", "")
-				s= s.replace("\t", " ")
-				print(f"print(\"{s}\")")
-				g.write(f"print(\"{s}\")\n")
-			g.close
+			if nyelv == "p":
+				for sor in f:				
+					s = sor.replace("\n", "")
+					s= s.replace("\t", " ")
+					print(f"print(\"{s}\")")
+					g.write(f"print(\"{s}\")\n")
+				g.close
+			elif nyelv == "cs":
+				for sor in f:				
+					s = sor.replace("\n", "")
+					s= s.replace("\t", " ")
+					print(f"Console.WriteLine(\"{s}\")")
+					g.write(f"Console.WriteLine(\"{s}\")\n")
+				g.close
 
-ToPrint()
+ToPrint(input("Milyen nyelven szeretnéd a kiírást (\"cs\", \"p\"):   "))
